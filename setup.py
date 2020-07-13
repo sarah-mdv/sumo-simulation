ab344713455342653e2f6365f0fcf14d24d5fa53
moyde <sarah.moydevitry@epfl.ch>
2020-07-13 08:26:07 +0200

    Project init files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='sumo-simulation',
+      version='0.1',
+      description='Easy data retrieval interface from sumo simulation',
+      author='Sarah Moy de Vitry',
+      license='MIT',
+      packages=['src'],
+      zip_safe=False)
